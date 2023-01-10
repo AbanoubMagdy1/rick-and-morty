4b7b3c7607b51845a45fe2575a74013cbfb48e0e
AbanoubMagdy1 <bebomagdy666@gmail.com>
2023-01-10 21:07:29 +0200

    ui: change color of info property and make it bold

--- src/components/CharacterInfo/CharacterInfo.tsx
+++ src/components/CharacterInfo/CharacterInfo.tsx
@@ -1,5 +1,6 @@
 import { Divider, Typography } from '@mui/material';
 import React from 'react'
+import './CharacterInfo.scss'
 
 
 interface Props {
@@ -13,7 +14,7 @@ function CharacterInfo({text, data, isLast, ...rest}: Props) {
   return (
     <>
         <Typography variant="h6" component="p" {...rest}>
-            {text}: {data || 'Unknown'}
+            <span className='info-property'>{text}:</span> {data || 'Unknown'}
         </Typography>
         {!isLast && <Divider/>}
     </>
