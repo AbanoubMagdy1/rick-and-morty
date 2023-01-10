4ff62e74c22136cf453492f77a3f1ed0391849bf
AbanoubMagdy1 <bebomagdy666@gmail.com>
2023-01-10 21:50:27 +0200

    test async handler component

---
+++ src/components/AsyncHandler/AsyncHandler.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import AsyncHandler from './AsyncHandler';
+
+describe('AsyncHandler', () => {
+    test('Show loader only on loading', () => {
+        render(<AsyncHandler loading={true} error="Error" children={<div data-testid="div"/>}/>)
+        expect(screen.getByRole('progressbar')).toBeInTheDocument()
+        expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+        expect(screen.queryByTestId('div')).not.toBeInTheDocument()
+    })
+
+    test('Show Error only', () => {
+        render(<AsyncHandler loading={false} error="Error" children={<div data-testid="div"/>}/>)
+        expect(screen.queryByRole('progressbar')).not.toBeInTheDocument()
+        expect(screen.queryByRole('alert')).toBeInTheDocument()
+        expect(screen.queryByTestId('div')).not.toBeInTheDocument()
+    })
+
+    test('Show Error only', () => {
+        render(<AsyncHandler loading={false} error="" children={<div data-testid="div"/>}/>)
+        expect(screen.queryByRole('progressbar')).not.toBeInTheDocument()
+        expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+        expect(screen.queryByTestId('div')).toBeInTheDocument()
+    })
+})
